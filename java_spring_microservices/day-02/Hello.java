157816196da9259c9b9748842be56686bc159ac5
BinitaDuttaDec03 <binitadutta777@gmail.com>
2025-11-11 23:04:30 +0530

    Day 2: - Primitive Types

---
+++ java_spring_microservices/day-02/Hello.java
@@ -0,0 +1,25 @@
+// Notes 1
+// class Hello{
+//     public static void main(String args[]){
+//         int a=10;
+//         int b=20;
+//         System.out.print(a+b);
+//     }
+// }
+
+// Notes 2
+class Hello{
+    public static void main(String args[]){
+        int a=9;
+        byte b=127;
+        short c=558;
+        long d=764384l;
+
+        float e=3.445f;
+        double f=3.55;
+
+        char g='g';
+
+        boolean h=true;
+    }
+}

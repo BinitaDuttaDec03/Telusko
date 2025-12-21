766596f7ef847fdf54f9093fa1a384c3372b1cea
BinitaDuttaDec03 <binitadutta777@gmail.com>
2025-12-21 10:50:54 +0530

    delete entry

--- java_spring_genai_udemy/03_hibernate/src/main/java/com/binita/Main.java
+++ java_spring_genai_udemy/03_hibernate/src/main/java/com/binita/Main.java
@@ -9,10 +9,8 @@ import org.hibernate.cfg.Configuration;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
-        Student s1 = new Student();
-        s1.setRollNo(104);
-        s1.setName("Navin");
-        s1.setMarks(94); // updating for Navin
+        Student s1 = null;
+
 
         SessionFactory sf = new Configuration()
                 .addAnnotatedClass(com.binita.Student.class)
@@ -20,9 +18,11 @@ public class Main {
                 .buildSessionFactory();
         Session session = sf.openSession();
 
-        Transaction transaction=session.beginTransaction();
+        Transaction transaction = session.beginTransaction();
 
-        session.merge(s1);
+        s1 = session.find(Student.class, 105);
+
+        session.remove(s1);
         transaction.commit();
 
         sf.close();
